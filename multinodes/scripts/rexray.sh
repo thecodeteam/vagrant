fe4f5a58fb967a812aa3577fd7b1fcbe9af6f579
jonasrosland <jonas.rosland@gmail.com>
2015-12-10 12:38:16 -0500

    Updating environment variables for REX-Ray 0.3

--- multinodes/scripts/rexray.sh
+++ multinodes/scripts/rexray.sh
@@ -1,6 +1,6 @@
 echo 'REXRAY_STORAGEDRIVERS=ec2' >> /etc/environment
-echo 'AWS_ACCESS_KEY='$1 >> /etc/environment
-echo 'AWS_SECRET_KEY='$2 >> /etc/environment
+echo 'AWS_ACCESSKEY='$1 >> /etc/environment
+echo 'AWS_SECRETKEY='$2 >> /etc/environment
 curl -sSL https://dl.bintray.com/emccode/rexray/install | sh -
 #cp /vagrant/scripts/conf_templates/rexray.conf /etc/init/rexray.conf
 service rexray start
